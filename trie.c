3a813b1fd182f532a74281fd107a9690d32a59a8
nokhlopkov <tenouji@gmail.com>
2018-07-21 17:50:28 +0300

    Update trie.c

--- trie.c
+++ trie.c
@@ -16,15 +16,25 @@ int insert(char* key, trie* root)
     
     for (ptr = root; keypos < keylen; ptr = ptr->path[abs(key[keypos] - 'a')], keypos++)
     {
-        trie* branch = malloc(sizeof(trie));
-        ptr->path[abs(key[keypos] - 'a')] = branch;
+        if (!ptr->path[abs(key[keypos] - 'a')])
+        {
+            trie* branch = malloc(sizeof(trie));
+            if (!branch)
+                return 0;
+            
+            ptr->path[abs(key[keypos] - 'a')] = branch;
+        }
     }
     
-    ptr->word = malloc(strlen(key) + 1);
-    ptr->word = strcpy(ptr->word, key);
-    ptr->word[strlen(key)] = '\0';
-    
-    return 1;
+    if (strcmp(ptr->word, key) != 0)
+    {
+        ptr->word = malloc(strlen(key) + 1);
+        ptr->word = strcpy(ptr->word, key);
+        ptr->word[strlen(key)] = '\0';
+        return 1;
+    }
+    else
+        return 0;
 }
 
 int search(const char* key, const trie* root)
